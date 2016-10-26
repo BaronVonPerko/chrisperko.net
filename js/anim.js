33c6bbd9853eef84bc9fd3a0bcbce399cad062bb
Chris Perko <cperko@teachers-teachers.com>
2016-10-25 21:49:54 -0500

    First work on the winter rework

--- js/anim.js
+++ js/anim.js
@@ -1,6 +1,6 @@
 
 $(window).scroll(function() {
-  if($(window).scrollTop() > 850) {
-    $('.item').removeClass('hidden');
-  }
+  // if($(window).scrollTop() > 850) {
+  //   $('.item').removeClass('hidden');
+  // }
 });
