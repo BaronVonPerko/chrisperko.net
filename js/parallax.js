34d681ec3f1e1cd6bcb0c23cb3da809fe8cebb57
Chris Perko <cperko@teachers-teachers.com>
2016-11-01 21:23:58 -0500

    more mobile tweaks

--- js/parallax.js
+++ js/parallax.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var pxFromTop = $('body').scrollTop();
     var bgPos = pxFromTop * 0.5;
     var pos = '0 ' + bgPos + 'px';
-    
+
     $('body').css({'background-position' : pos});
     $('header').css({'background-position' : pos});
   });
